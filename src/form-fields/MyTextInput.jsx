e35d5df36d7bd9c879ce581c45bef649847d29fb
Amos <asidelnik@gmail.com>
2023-06-05 17:47:09 +0300

    Change the input border color to red when a field has an error and isn’t focused

--- src/form-fields/MyTextInput.jsx
+++ src/form-fields/MyTextInput.jsx
@@ -3,16 +3,17 @@ import { useField } from "formik";
 export default function MyTextInput({ label, ...props }) {
   // TODO - Learn this
   const [field, meta] = useField(props);
+  const isError = meta.touched && meta.error;
+  const isFocused = meta.touched && meta.error && meta.active;
   return (
     <>
       <label htmlFor={props.id || props.name}>{label}</label>
       <input
         {...field}
         {...props}
+        className={isError && !isFocused ? "input-error" : null}
       />
-      {meta.touched && meta.error ? (
-        <div className="error">{meta.error}</div>
-      ) : null}
+      {isError ? <div className="error">{meta.error}</div> : null}
     </>
   );
 }
